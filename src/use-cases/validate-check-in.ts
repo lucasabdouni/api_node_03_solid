bb1bf21d477de562432f1012b5bd4997fb42459b
lucasabdouni <lucasabdouni@hotmail.com>
2023-10-02 21:47:11 -0300

    create use cases for validate-check-in  and yous tests

---
+++ src/use-cases/validate-check-in.ts
@@ -0,0 +1,30 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface ValidateCheckInUseCaseRequest {
+  checkInId: string
+}
+
+interface ValidateCheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+export class ValidateCheckInUseCase {
+  constructor(private CheckInsRepository: CheckInsRepository) {}
+
+  async execute({
+    checkInId,
+  }: ValidateCheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponse> {
+    const checkIn = await this.CheckInsRepository.findById(checkInId)
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError()
+    }
+
+    checkIn.validated_at = new Date()
+
+    await this.CheckInsRepository.save(checkIn)
+
+    return { checkIn }
+  }
+}
