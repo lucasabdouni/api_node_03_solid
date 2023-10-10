6e5228efcce58705a02f99ce85b16f28f2256ccb
lucasabdouni <lucasabdouni@hotmail.com>
2023-10-09 21:31:13 -0300

    created refresh token and rbac

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,6 +3,7 @@ import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { create } from './create'
 import { nearby } from './neaby'
 import { search } from './search'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
@@ -10,5 +11,5 @@ export async function gymsRoutes(app: FastifyInstance) {
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
 
-  app.post('/gyms', create)
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
